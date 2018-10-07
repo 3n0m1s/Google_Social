075c6d96a0d17bac8186a830992482497d883611
Simone Schiavolini <simone.schiavolini@progettispeciali.net>
2018-10-07 14:17:45 +0200

    Add files via upload

---
+++ GoogleAuthWinForm/Access.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace GoogleAuthWinForm
+{
+    partial class Access
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAuth = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.AccessToken = new System.Windows.Forms.Label();
+            this.Expire = new System.Windows.Forms.Label();
+            this.refreshToken = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAuth
+            // 
+            this.btnAuth.Location = new System.Drawing.Point(605, 137);
+            this.btnAuth.Name = "btnAuth";
+            this.btnAuth.Size = new System.Drawing.Size(75, 23);
+            this.btnAuth.TabIndex = 0;
+            this.btnAuth.Text = "Login";
+            this.btnAuth.UseVisualStyleBackColor = true;
+            this.btnAuth.Click += new System.EventHandler(this.btnAuth_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Access Token";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Refresh Token";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Expires";
+            // 
+            // AccessToken
+            // 
+            this.AccessToken.AutoSize = true;
+            this.AccessToken.Location = new System.Drawing.Point(96, 12);
+            this.AccessToken.Name = "AccessToken";
+            this.AccessToken.Size = new System.Drawing.Size(35, 13);
+            this.AccessToken.TabIndex = 4;
+            this.AccessToken.Text = "label4";
+            // 
+            // Expire
+            // 
+            this.Expire.AutoSize = true;
+            this.Expire.Location = new System.Drawing.Point(96, 63);
+            this.Expire.Name = "Expire";
+            this.Expire.Size = new System.Drawing.Size(35, 13);
+            this.Expire.TabIndex = 5;
+            this.Expire.Text = "label4";
+            // 
+            // refreshToken
+            // 
+            this.refreshToken.AutoSize = true;
+            this.refreshToken.Location = new System.Drawing.Point(96, 40);
+            this.refreshToken.Name = "refreshToken";
+            this.refreshToken.Size = new System.Drawing.Size(35, 13);
+            this.refreshToken.TabIndex = 6;
+            this.refreshToken.Text = "label4";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 91);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Email";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(96, 91);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "email";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(19, 108);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Nome";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(99, 108);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(33, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "nome";
+            // 
+            // Access
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 172);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.refreshToken);
+            this.Controls.Add(this.Expire);
+            this.Controls.Add(this.AccessToken);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAuth);
+            this.Name = "Access";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Access_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAuth;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label AccessToken;
+        private System.Windows.Forms.Label Expire;
+        private System.Windows.Forms.Label refreshToken;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ GoogleAuthWinForm/Access.cs
@@ -0,0 +1,88 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Windows.Forms;
+
+namespace GoogleAuthWinForm
+{
+    public partial class Access : Form
+    {
+        public string email,nome;
+
+        public Access()
+        {
+            InitializeComponent();
+
+            AccessToken.Text = "";
+            refreshToken.Text = "";
+            Expire.Text = "";
+        }
+
+        private void btnAuth_Click(object sender, EventArgs e)
+        {
+            Auth m = new Auth();
+            var result = m.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                AccessToken.Text = m.access.Access_token;
+                refreshToken.Text = m.access.refresh_token;
+
+                if (DateTime.Now < m.access.created.AddHours(1))
+                {
+                    Expire.Text = m.access.created.AddHours(1).Subtract(DateTime.Now).Minutes.ToString();
+                    getgoogleplususerdataSer(AccessToken.Text);
+                }
+            }
+
+
+        }
+
+        private void Access_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void getgoogleplususerdataSer(string access_token)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                var urlProfile = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=" + access_token;
+
+                client.CancelPendingRequests();
+                HttpResponseMessage output = await client.GetAsync(urlProfile);
+
+                if (output.IsSuccessStatusCode)
+                {
+                    string outputData = await output.Content.ReadAsStringAsync();
+                    GoogleUserOutputData serStatus = JsonConvert.DeserializeObject<GoogleUserOutputData>(outputData);
+
+                    if (serStatus != null)
+                    {
+                        nome = serStatus.name;
+                        email = serStatus.email;
+                        this.label5.Text = email;
+                        this.label7.Text = nome;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex);
+                //catching the exception
+            }
+        }
+
+        public class GoogleUserOutputData
+        {
+            public string id { get; set; }
+            public string name { get; set; }
+            public string given_name { get; set; }
+            public string email { get; set; }
+            public string picture { get; set; }
+        }
+
+    }
+}
